95337667c393d7b8accaac9dedde51c95d5a51a2
Josh Overly <joshoverly@yahoo.com>
2016-03-22 14:38:07 -0700

    add checkboxes and complete app functionality

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,31 +2,35 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
+import {DonePipe} from './done.pipe';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
   outputs: ['onTaskSelect'],
-  directives: [TaskComponent],
-  template: `
-    <task-display *ngFor="#currentTask of taskList"
-      (click)="taskClicked(currentTask)"
-      [class.selected]="currentTask === selectedTask"
-      [task]="currentTask">
-    </task-display>
-    <edit-task-details *ngIf="selectedTask" [task]="selectedTask"></edit-task-details>
-  `
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
+  pipes: [DonePipe],
+  templateUrl: 'app/task-list.component.html'
 })
 export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
   public selectedTask: Task;
+  public filterDone: string = "notDone";
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
-    console.log("child", clickedTask);
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
+  }
+  onChange(filterOption) {
+    this.filterDone = filterOption;
   }
 }
